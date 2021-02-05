9886fc219126bc936352b7b45032796ee12ac1ff
BURCU AYDIN <burcu.aydin@stu.thk.edu.tr>
2021-02-05 15:22:06 +0300

    add init

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+
+            myDictionary.Add(1, "Bardak");
+            myDictionary.Add(2, "Kaşık");
+            myDictionary.Add(3, "Tabak");
+
+            myDictionary.List();
+        }
+
+        
+    }
+}
